abdebbadcac48fda23f8fc067b6afcffa15f5329
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-06-23 23:11:28 +0530

    Create Intersection_LL.cpp

---
+++ Linked-List/Intersection_LL.cpp
@@ -0,0 +1,38 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+*****************************************************************/
+
+Node* findIntersection(Node *firstHead, Node *secondHead)
+{
+    //Write your code here
+    Node *a=firstHead,*b=secondHead;
+    while(a!=b){
+        a=(a!=NULL)?a->next:secondHead;
+        b=(b!=NULL)?b->next:firstHead;
+    }
+    return b;
+}
