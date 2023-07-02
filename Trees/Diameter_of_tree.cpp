dcafeec384444d44fed39bddefc7a6068d096c3c
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-07-02 23:46:59 +0530

    Create Diameter_of_tree.cpp

---
+++ Trees/Diameter_of_tree.cpp
@@ -0,0 +1,32 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+int findMax(TreeNode<int>*root,int &maxi){
+    if(root==NULL) return 0;
+    int lh= findMax(root->left,maxi);
+    int rh= findMax(root->right,maxi);
+    maxi=max(maxi,lh+rh);
+    return 1+max(lh,rh);
+}
+int diameterOfBinaryTree(TreeNode<int> *root)
+{    int maxi=0;
+	 int height= findMax(root,maxi);
+    return maxi;
+}
